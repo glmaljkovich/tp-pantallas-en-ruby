f9d2a27845bfd037e44503c0435f9802d7279646
dementlier <forceg.001@gmail.com>
2016-03-30 01:02:41 -0300

    Se agregó la clase PC, SSD y disco duro

--- model/pc/PC.rb
+++ model/pc/PC.rb
@@ -1,10 +1,13 @@
 class PC
   def initialize(pantalla, placa_de_video)
-    @pantalla = pantalla
+    @pantalla       = pantalla
     @placa_de_video = placa_de_video
   end
   def apta_para_juegos?
     pantalla.puede_usarse_para_juegos? && placa_de_video.puede_usarse_para_juegos?
+  end
+  def consumo
+    pantalla.consumo + placa_de_video.consumo
   end
   attr_accessor :pantalla, :placa_de_video
 end
@@ -14,7 +17,7 @@ class DiscoDuro
     @rpm = rpm
   end
   def consumo
-    rpm * 60 / 1000
+    super + rpm * 60 / 1000
   end
 end
 
@@ -22,5 +25,7 @@ class SSD
   def initialize(gasto)
     @consumo = gasto
   end
-  attr_accessor :consumo
+  def consumo
+    super + consumo
+  end
 end
